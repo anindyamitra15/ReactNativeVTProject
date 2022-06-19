b8529006390c50edafb8a0c3f866a412e7233b0f
Aryan <aryanrot234@gmail.com>
2022-06-20 00:23:15 +0530

    updated the message signout func

--- ChatApp/src/pages/Message.js
+++ ChatApp/src/pages/Message.js
@@ -8,17 +8,20 @@ import {
   TextInput,
   Platform,
   ToastAndroid,
+  TouchableHighlight
 } from "react-native";
 import React, { useRef, useState, useEffect } from "react";
 
 import { Button } from "react-native-elements";
-import Icons from "react-native-vector-icons/Ionicons";
+// import Icons from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import data from "../Data/data";
 import MessageList from "../components/MessageList";
 import { getCurrentUser, signUserOut, auth } from "../../firebase";
+import Icons from "react-native-vector-icons/Ionicons";
+import Material from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Message = ({ navigation }) => {
+const Message = ({ navigation , time, isLeft,messageing }) => {
   const [chat, setChat] = useState();
   const [message, setMessage] = useState("");
 
@@ -32,18 +35,89 @@ const Message = ({ navigation }) => {
     }
   };
 
+
+  const isOnLeft = (type) => {
+		if (isLeft && type === "messageContainer") {
+			return {
+				alignSelf: "flex-start",
+				backgroundColor: "#f0f0f0",
+				borderTopLeftRadius: 0,
+			};
+		} else if (isLeft && type === "messageing") {
+			return {
+				color: "#000",
+			};
+		} else if (isLeft && type === "time") {
+			return {
+				color: "darkgray",
+			};
+		} else {
+			return {
+				borderTopRightRadius: 0,
+			};
+		}
+	};
+
   return (
     <View style={styles.maincontainer}>
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#fff",
+          elevation: 3,
+          marginTop: 10,
+          padding: 17,
+          paddingTop: 60,
+          top: -10,
+          
+          borderTopLeftRadius: 15,
+          borderBottomLeftRadius: 15,
+          borderTopRightRadius: 15,
+          borderBottomRightRadius: 15,
+        }}
+      >
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>QuickTalks</Text>
+        <TouchableHighlight
+          style={{
+            borderRadius:
+              Math.round(
+                Dimensions.get("window").width + Dimensions.get("window").height
+              ) / 2,
+            width: 10,
+            height: 10,
+            marginTop: 10,
+            backgroundColor: "#416EE7",
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: 10,
+          }}
+        >
+          <Text></Text>
+        </TouchableHighlight>
+        <View style={{ flexDirection: "row", left: 140 }}>
+          
+          <TouchableOpacity style={{ marginLeft: 70 }} 
+        onPress={signout}
+          
+          >
+            <Material
+              name="logout"
+              size={25}
+              style={{ flexDirection: "row" }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+
       <ScrollView
         style={{ paddingVertical: 10, marginVertical: 5, flex: 1 }}
-      ></ScrollView>
-
-      <TouchableOpacity
-        style={{ alignItems: "center", justifyContent: "center", top: -100 }}
-        onPress={signout}
       >
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
+
+
+      </ScrollView>
+
+     
 
       <View style={styles.innerContainer}>
         <View style={styles.inputAndMicrophone}>

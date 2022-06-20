6aa74353b3f8023ad90b6b135b62886025951bf5
Anindya Mitra <anindya15l12@gmail.com>
2022-06-20 11:42:44 +0530

    android configured

--- ChatApp/firebase.js
+++ ChatApp/firebase.js
@@ -1,44 +1,24 @@
 import { useState } from "react";
+import auth from '@react-native-firebase/auth';
 
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithCredential,
-  signOut,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
 
-import {
-  ref,
-  getFirestore,
-  collection,
-  doc,
-  orderBy,
-  limit,
-  setDoc,
-  getDoc,
-  getDocs,
-} from "firebase/firestore";
+function collection(param1, param2) {
+
+}
+
+const db = null
+
+function getDocs(param) {
+
+}
+
+function getAuth() {
+
+}
+
 
 import { ToastAndroid } from "react-native";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDl-xX016yxmIC94ED6Tn9rGs9oxZ26vTc",
-  authDomain: "reactnativevtproject.firebaseapp.com",
-  databaseURL:
-    "https://reactnativevtproject-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "reactnativevtproject",
-  storageBucket: "reactnativevtproject.appspot.com",
-  messagingSenderId: "177988330912",
-  appId: "1:177988330912:web:fc27988802c0bd291370c5",
-};
-
-//initialize firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore(app);
 
 export const signUp = async (email, password, name, phone) => {
   try {
@@ -68,7 +48,8 @@ export const signIn = async (email = "", password = "") => {
 };
 
 export function getCurrentUser() {
-  return getAuth().currentUser;
+  // return getAuth().currentUser;
+  return undefined
 }
 
 export function signUserOut() {
@@ -89,14 +70,14 @@ export function getMessages() {
   // const query = messageRef.orderBy("createdAt")
   const output = {};
   
-  getDocs(messagesRef)
-    .then((docu) => {
-      docu.docs.forEach((docSnap) => {
-        output[docSnap.id] = docSnap.data();
-      });
-      console.log(output);
-    })
-    .catch((error) => console.log(error));
+  // getDocs(messagesRef)
+  //   .then((docu) => {
+  //     docu.docs.forEach((docSnap) => {
+  //       output[docSnap.id] = docSnap.data();
+  //     });
+  //     console.log(output);
+  //   })
+  //   .catch((error) => console.log(error));
 
   // } catch (error) {
   //   return error.message;
